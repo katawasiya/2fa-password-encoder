e939cd4279a007acaa207eb9336115a3177cfcb7
Denis Sermyagin <d.sermyagin@kazanexpress.tech>
2024-04-04 01:09:30 +0300

    feat(qr): added qr code for secret, but dont't work with google auth

--- pckg/secrets/secret_key.go
+++ pckg/secrets/secret_key.go
@@ -1,19 +1,25 @@
 package secretKey
 
 import (
+	"encoding/base32"
 	"fmt"
 	"math/rand"
+	"os"
+
+	"github.com/mdp/qrterminal/v3"
 )
 
 type SecretKeyStruct struct {
 	SecretKey    string
 	SecretLength int
+	QRCode       string
 }
 
 type SecretKeyInterface interface {
 	RandomSecretLength() *SecretKeyStruct
 	GenerateSecretKey() *SecretKeyStruct
 	InitializeSecret() *SecretKeyStruct
+	CreateQRCode(label, issuer string) *SecretKeyStruct
 }
 
 func (s *SecretKeyStruct) RandomSecretLength() *SecretKeyStruct {
@@ -37,6 +43,40 @@ func InitializeSecret() *SecretKeyStruct {
 	s := &SecretKeyStruct{}
 	s.RandomSecretLength()
 	s.GenerateSecretKey()
-	fmt.Print("Secret Key: ", s.SecretKey, "\n")
+	s.CreateQRCode("encrypt", "katawasiya")
 	return s
+}
+
+// type TwoFactorInterface interface {
+// 	GetQRCode() string
+// }
+
+func (s *SecretKeyStruct) CreateQRCode(label, issuer string) (*SecretKeyStruct, error) {
+	// Создаем URI для Google Authenticator
+	secret := s.SecretKey
+	fmt.Println("Original Secret:", secret)
+
+	// Преобразуем секретный ключ в base32
+	ConvertToBase32 := func(s string) string {
+		b := []byte(s)
+		base32Secret := base32.StdEncoding.EncodeToString(b)
+		return base32Secret
+	}
+
+	base32Secret := ConvertToBase32(secret)
+	fmt.Println("Secret in base32:", base32Secret)
+
+	uri := fmt.Sprintf("otpauth://totp/%s?secret=%s&issuer=%s", label, base32Secret, issuer)
+
+	// Выводим QR-код в терминал
+	config := qrterminal.Config{
+		Level:     qrterminal.M,
+		Writer:    os.Stdout,
+		BlackChar: qrterminal.BLACK,
+		WhiteChar: qrterminal.WHITE,
+		QuietZone: 1,
+	}
+	qrterminal.GenerateWithConfig(uri, config)
+
+	return s, nil
 }
